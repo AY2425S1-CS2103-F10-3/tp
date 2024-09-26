d8f9f834269b75732347eb245f36ab0a78efd69d
Joel Tio <joeltiojinhon@gmail.com>
2024-09-26 23:19:15 +0800

    Add test for RemarkCommand with filtered list

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,6 +1,11 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BASEBALL;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_COOKING;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -43,5 +48,22 @@ public class RemarkCommandTest {
         RemarkCommand remarkCommand = new RemarkCommand(outOfBoundIndex, VALID_REMARK_BASEBALL);
 
         assertCommandFailure(remarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Person personInFilteredList = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person remarkedPerson = new PersonBuilder(personInFilteredList).withRemark(VALID_REMARK_COOKING).build();
+        RemarkCommand remarkCommand = new RemarkCommand(INDEX_FIRST_PERSON, VALID_REMARK_COOKING);
+
+        String expectedMessage = String.format(RemarkCommand.MESSAGE_REMARK_PERSON_SUCCESS,
+                Messages.format(remarkedPerson));
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), remarkedPerson);
+
+        assertCommandSuccess(remarkCommand, model, expectedMessage, expectedModel);
     }
 }
